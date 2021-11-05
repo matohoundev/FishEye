66bf73e2959dbbeaa2cca2d127285987aea22e14
matohoundev <matohoundev@gmail.com>
2021-11-05 10:44:22 +0100

    end select css

---
+++ js/select/select.js
@@ -0,0 +1,24 @@
+const select = document.querySelector(".select");
+const blocLinks = document.querySelector(".bloc-links");
+const btnDrop = document.querySelector(".btn-top");
+const liItems = document.querySelectorAll(".dropdown li");
+const svg = document.querySelector("#Layer_1");
+
+let toggleIndex;
+
+btnDrop.addEventListener("click", toggleDropDown);
+
+function toggleDropDown() {
+  if (!toggleIndex) {
+    blocLinks.style.height = `${blocLinks.scrollHeight}px`;
+    svg.style.transform = `rotate(180deg)`;
+    toggleIndex = true;
+    return;
+  }
+
+  svg.style.transform = `rotate(0deg)`;
+  blocLinks.style.height = 0;
+  toggleIndex = false;
+}
+
+liItems.forEach((li) => li.addEventListener("click", toggleDropDown));
