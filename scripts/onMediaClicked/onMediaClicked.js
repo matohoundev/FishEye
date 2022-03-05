45452b0fe4aa2f27c7ba9ec68afb1d96ab16beb4
matohoundev <matohoundev@gmail.com>
2022-03-05 15:08:25 +0100

    fix click on click

--- scripts/onMediaClicked/onMediaClicked.js
+++ scripts/onMediaClicked/onMediaClicked.js
@@ -1,16 +1,18 @@
-// import Likes from '../likes/likes.js';
-// import modalPictures from '../modalPictures/modalPictures.js';
-
 import Likes from '../likes/likes.js';
 import modalPictures from '../modalPictures/modalPictures.js';
 
 export default function onMediaClicked(medias, media, photographerName, e) {
     let selectedImg = e.target.classList.contains('card-photography__img');
     let selectedVideo = e.target.classList.contains('card-photography__video');
-    let keyboardClickedOnEnter = e.target.firstElementChild.localName;
+    let keyboardClickedOnEnter;
+
+    // if click or use keyboard
+    if (e.target.firstElementChild) {
+        keyboardClickedOnEnter = e.target.firstElementChild.localName;
+    }
     
-    if (selectedImg || keyboardClickedOnEnter === 'img' ) {
-        new modalPictures().init(medias, media, photographerName, keyboardClickedOnEnter);
+    if (selectedImg || keyboardClickedOnEnter === 'img') {
+        new modalPictures().init(medias, media, photographerName, 'img');
     } else if (selectedVideo || keyboardClickedOnEnter === 'video') {
         new modalPictures().init(medias, media, photographerName);
     } else {
